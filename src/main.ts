dc23926e00f7a57802af2ff8e685d047c7940bc0
wdse21 <95352784+wdse21@users.noreply.github.com>
2025-06-05 23:45:27 +0900

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,16 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { HttpExceptionFilter } from './common/middlewares/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { winstonLogger } from './common/loggers/winston';
+
+declare const module: any;
 
 async function bootstrap() {
-  // logger 로그 레벨 참고: https://cdragon.tistory.com/entry/NestJS-Logging-%EC%95%8C%EC%95%84%EB%B3%B4%EA%B8%B0-feat-winston
   const app = await NestFactory.create(AppModule, {
-    logger:
-      process.env.NODE_ENV === 'production'
-        ? ['error', 'warn', 'log']
-        : ['error', 'warn', 'log', 'verbose', 'debug'],
+    bufferLogs: false,
+    logger: winstonLogger,
   });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -24,5 +25,10 @@ async function bootstrap() {
   console.log(
     `현재 애플리케이션 실행 환경: ${configService.get<number>('nodeEnv')}, ${configService.get<number>('port')}`,
   );
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();

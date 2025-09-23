692990a96212b29cae33156795c9ebfce303081e
wdse21 <95352784+wdse21@users.noreply.github.com>
2025-09-23 21:58:51 +0900

    Add files via upload

---
+++ migrations/data-source.ts
@@ -0,0 +1,19 @@
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+import { DataSource } from 'typeorm';
+
+config();
+const configService = new ConfigService();
+
+export default new DataSource({
+  type: 'postgres',
+  host: configService.get('DB_HOST'),
+  port: configService.get('DB_PORT'),
+  username: configService.get('DB_USER'),
+  password: configService.get('DB_PASS'),
+  database: configService.get('DB_NAME'),
+  synchronize: false,
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['./migrations/*.ts'],
+  migrationsTableName: 'migrations',
+});
